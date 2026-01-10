5e01fa65349b119c56db59f2d193e0aaffff2401
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2026-01-10 18:19:09 +0100

    edit routing and main structure

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../shared/header/header.component";
+
+@Component({
+  selector: 'app-main',
+  imports: [HeaderComponent],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+}
