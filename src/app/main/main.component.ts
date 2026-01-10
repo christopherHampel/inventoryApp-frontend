ece390e68f76f0394a3334c7bff63ed7689278a1
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2026-01-10 21:23:23 +0100

    add inventory list and header stick position

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../shared/header/header.component";
+import { InventoryListComponent } from "./inventory-list/inventory-list.component";
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, InventoryListComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
