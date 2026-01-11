952ff52f5af971077967f619561ba24d4c3e1005
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2026-01-11 17:40:28 +0100

    add firebase project and add-item-dialog at inventory-list component

---
+++ src/app/main/inventory-list/add-item-dialog/add-item-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-item-dialog',
+  imports: [],
+  templateUrl: './add-item-dialog.component.html',
+  styleUrl: './add-item-dialog.component.scss'
+})
+export class AddItemDialogComponent {
+
+}
--- src/app/main/inventory-list/inventory-list.component.ts
+++ src/app/main/inventory-list/inventory-list.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, model, signal } from '@angular/core';
 import { SingleItemComponent } from './single-item/single-item.component';
+import { MatDialog } from '@angular/material/dialog';
+import { AddItemDialogComponent } from './add-item-dialog/add-item-dialog.component';
+AddItemDialogComponent
 
 @Component({
   selector: 'app-inventory-list',
-  imports: [ SingleItemComponent ],
+  imports: [SingleItemComponent ],
   templateUrl: './inventory-list.component.html',
-  styleUrl: './inventory-list.component.scss'
+  styleUrl: './inventory-list.component.scss',
 })
 export class InventoryListComponent {
+  readonly animal = signal('');
+  readonly name = model('');
+  readonly dialog = inject(MatDialog);
 
+  openDialog(): void {
+    const dialogRef = this.dialog.open(AddItemDialogComponent, {
+      data: { name: this.name(), animal: this.animal() },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      if (result !== undefined) {
+        this.animal.set(result);
+      }
+    });
+  }
 }
