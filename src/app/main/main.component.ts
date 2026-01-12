2acc88a87a41e670e1a33472e4a7f941967a2e71
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2026-01-12 18:16:44 +0100

    add sidebar and change main styling

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../shared/header/header.component";
 import { InventoryListComponent } from "./inventory-list/inventory-list.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent, InventoryListComponent],
+  imports: [HeaderComponent, InventoryListComponent, SidebarComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
