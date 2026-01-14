4890d09c06bb5bf602c264ddf3063e4750e91c36
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2026-01-14 21:10:02 +0100

    edit add-item-dialog

--- src/app/main/inventory-list/add-item-dialog/add-item-dialog.component.ts
+++ src/app/main/inventory-list/add-item-dialog/add-item-dialog.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogRef } from '@angular/material/dialog';
+import { InventoryListComponent } from '../inventory-list.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogClose } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-item-dialog',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogClose,
+  ],
   templateUrl: './add-item-dialog.component.html',
-  styleUrl: './add-item-dialog.component.scss'
+  styleUrl: './add-item-dialog.component.scss',
 })
 export class AddItemDialogComponent {
+  readonly dialogRef = inject(MatDialogRef<InventoryListComponent>);
+  productName:string = '';
 
+  addProduct() {
+    console.log(this.productName);
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
--- src/app/main/inventory-list/inventory-list.component.ts
+++ src/app/main/inventory-list/inventory-list.component.ts
@@ -5,6 +5,7 @@ import { AddItemDialogComponent } from './add-item-dialog/add-item-dialog.compon
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 
+
 @Component({
   selector: 'app-inventory-list',
   imports: [SingleItemComponent, MatFormFieldModule, MatIconModule ],
@@ -12,20 +13,20 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './inventory-list.component.scss',
 })
 export class InventoryListComponent {
-  readonly animal = signal('');
+  readonly productName = signal('');
   readonly name = model('');
   readonly dialog = inject(MatDialog);
 
   openDialog(): void {
     const dialogRef = this.dialog.open(AddItemDialogComponent, {
-      data: { name: this.name(), animal: this.animal() },
+      data: { name: this.name(), animal: this.productName() },
     });
 
     dialogRef.afterClosed().subscribe((result) => {
       console.log('The dialog was closed');
-      if (result !== undefined) {
-        this.animal.set(result);
-      }
+      // if (result !== undefined) {
+      //   this.animal.set(result);
+      // }
     });
   }
 }
